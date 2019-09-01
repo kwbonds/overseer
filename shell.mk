89fc2fe0c6bce8b5d086d6bc9ebe39608a7a37ee
Alberto Marchetti <cmaster11@gmail.com>
2019-09-01 09:34:20 +0300

    test actions

--- shell.mk
+++
@@ -1,7 +0,0 @@
-.PHONY: shell-build-overseer
-shell-build-overseer:
-	bash scripts/docker-build-hub.sh overseer
-
-.PHONY: shell-build-overseer-webhook-bridge
-shell-build-overseer-webhook-bridge:
-	bash scripts/docker-build-hub.sh overseer-webhook-bridge Dockerfile.webhook-bridge
