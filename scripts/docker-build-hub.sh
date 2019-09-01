275ba91fecbaf54b668a0f83476cf58b1ab861f5
Alberto Marchetti <cmaster11@gmail.com>
2019-09-01 11:09:36 +0300

    test actions

--- scripts/docker-build-hub.sh
+++ scripts/docker-build-hub.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -Eeuxmo pipefail
+set -e
 DIR="$(dirname "$(command -v greadlink >/dev/null 2>&1 && greadlink -f "$0" || readlink -f "$0")")"
 
 source "$DIR/docker-build.sh"
