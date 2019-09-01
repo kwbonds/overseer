ef5190d7d85f74b48203e2fb43d40399ad12ee6f
Alberto Marchetti <cmaster11@gmail.com>
2019-09-01 11:24:29 +0300

    test actions

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,8 +3,10 @@ set -e
 DIR="$(dirname "$(command -v greadlink >/dev/null 2>&1 && greadlink -f "$0" || readlink -f "$0")")"
 
 # --- Config
+DEFAULT_DOCKERFILE="$DIR/Dockerfile"
+
 IMAGE_NAME="${1:-overseer}"
-DOCKERFILE="${2:-Dockerfile}"
+DOCKERFILE="${2:-$DEFAULT_DOCKERFILE}"
 
 # ---
 VERSION=$(git describe --tags 2>/dev/null || echo 'master')
