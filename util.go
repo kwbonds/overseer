72f1f4b2da2cb35202f7c9c38b249c3c1f40b758
Alberto Marchetti <cmaster11@gmail.com>
2019-10-15 13:29:54 +0300

    typo

--- util.go
+++ util.go
@@ -11,7 +11,7 @@ func WaitForCtrlC() {
 	endWaiter := sync.WaitGroup{}
 	endWaiter.Add(1)
 	signalChannel := make(chan os.Signal, 1)
-	signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		<-signalChannel
 		endWaiter.Done()
