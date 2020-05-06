ab489cd1110ec47762e9eb316e6e9d792b805eb4
cmaster11 <cmaster11@gmail.com>
2020-05-06 08:46:11 +0300

    updated versions

--- example-kubernetes/enqueue.sh
+++ example-kubernetes/enqueue.sh
@@ -4,6 +4,6 @@ set -Eeuxmo pipefail
 RULE="$1"
 
 (echo "$RULE" | kubectl -n overseer run --restart=Never -i enqueue-manual \
-  --image="cmaster11/overseer:1.12.9" \
+  --image="cmaster11/overseer:1.12.10" \
   -- enqueue -redis-host redis:6379 - || true) &&
   kubectl -n overseer delete pod enqueue-manual
